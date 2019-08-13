8553515980f5f2ac7f544a1cc572e820d84266aa
judesama <judeagagad@gmail.com>
2019-08-13 14:08:08 +0800

    layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Menu, Icon, Button ,Layout} from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const { Header, Sider, Content , Footer} = Layout;
+
+class App extends React.Component {
+  
+
+
+  render(){
+
+    return (
+      <React.Fragment>
+          <Layout>
+             
+                
+              <Sider 
+                  trigger={null} 
+                  collapsible collapsed={true} 
+                  style={{
+                    
+                    background:'transparent',
+                  
+                  }}
+
+              >
+                <Menu mode="inline" 
+                      defaultSelectedKeys={['1']}
+                      style={{
+                        background:'white',
+                        height:'100vh',
+
+                      }} 
+
+                >
+
+                  <Menu.Item key="1">
+                    <Icon type="user" />
+                    <span>nav 1</span>
+                  </Menu.Item>
+                  <Menu.Item key="2">
+                    <Icon type="video-camera" />
+                    <span>nav 2</span>
+                  </Menu.Item>
+                  <Menu.Item key="3">
+                    <Icon type="upload" />
+                    <span>nav 3</span>
+                  </Menu.Item>
+                </Menu>
+
+              </Sider>
+
+              <Layout>
+                
+            
+                <Header 
+                    style={{ 
+                      background: '#444', 
+                      color:'#FFF',
+                      padding:0,
+
+                }}>
+                    <Icon type="menu-fold" size="large" style={{padding:15}} />
+                </Header>
+               
+  
+                <Content
+                  style={{
+                    // border:'solid 1px green'
+                  }}
+                >
+                  Content
+                </Content>
+
+
+                <Footer style={{border:'solid 1px blue'}} >footer</Footer>
+
+
+              </Layout>
+              
+          </Layout>
+
+          
+      </React.Fragment>
+    );
+  
+  }
 }
 
 export default App;
